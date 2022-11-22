c2e53170981f65f341ceee549bac21e2b4fc067d
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2022-11-21 17:07:22 -0800

    Init

---
+++ src/test/java/org/sfjs/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sfjs.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
